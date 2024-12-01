4123eb4d0a22dfbdde2ef8872bd6c36b89cbca2b
Anna Bernard <bernardanna2020@gmail.com>
2024-12-01 21:31:21 +0100

    Created SequenceClass and its methods (readFASTA, get_reverse, count)

--- libs/SequenceClass.cpp
+++ libs/SequenceClass.cpp
@@ -3,3 +3,65 @@
 //
 
 #include "SequenceClass.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+// complementMap is created here only temporary
+Sequence::Sequence() {
+    complementMap['A'] = 'T';
+    complementMap['T'] = 'A';
+    complementMap['C'] = 'G';
+    complementMap['G'] = 'C';
+}
+
+Sequence::Sequence(const std::string& filepath) {
+    readFASTA(filepath);
+};
+
+void Sequence::readFASTA(const std::string& filepath){
+    std::ifstream file(filepath);
+    if (!file.is_open()) {
+      std::cerr << filepath << " cannot be opend" << std::endl;
+      return;
+    }
+    std::string line;
+    bool isHeader = true;
+
+    while (std::getline(file, line)) {
+        if (line.empty()) {
+            continue;
+        }
+
+        if (isHeader && line[0] == '>') {
+            header = line.substr(1);
+            isHeader = false;
+        } else {
+            sequence += line;
+        }
+    }
+
+    file.close();
+}
+
+std::string Sequence::get_reverse() {
+    std::string reverseSeq = sequence;
+    std::reverse (reverseSeq.begin(), reverseSeq.end());
+    return reverseSeq;
+    }
+
+
+std::unordered_map<char, int> Sequence::count() {
+    std::unordered_map<char, int> charCount;
+
+    for (const auto& pair : complementMap) {
+        charCount[pair.first] = 0;
+    }
+
+    for (const char& base : sequence) {
+        if (complementMap.find(base) != complementMap.end()) {
+            charCount[base]++;
+        }
+    }
+    return charCount;
+}
--- libs/SequenceClass.h
+++ libs/SequenceClass.h
@@ -4,5 +4,30 @@
 
 #ifndef SEQUENCECLASS_H
 #define SEQUENCECLASS_H
+#include <string>
+#include <unordered_map>
+
+class Sequence {
+protected:
+    std::unordered_map<char, char> complementMap;
+public:
+    std::string header;
+    std::string sequence;
+
+    Sequence();
+    Sequence(const std::string& filepath);
+
+    // Method reads single sequence and its header from FASTA file
+    void readFASTA(const std::string& filepath);
+
+    // Method returns an unordered map, where the key is the character, and the value is the count of its occurrences in
+    // the sequence attribute. It depends on complementMap attribute.
+    std::unordered_map<char, int> count();
+
+    // Method returns reversed sequence
+    std::string get_reverse();
+
+
+};
 
 #endif //SEQUENCECLASS_H
