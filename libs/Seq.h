18974b73b3501d6a0d983098d140e4ad30112f0d
mmcharchuta <mmcharchuta@gmail.com>
2024-12-13 11:54:56 +0100

    Deleted redundant comments.

--- libs/Seq.h
+++ libs/Seq.h
@@ -22,11 +22,7 @@ public:
     // Method returns an unordered map, where the key is the character, and the value is the count of its occurrences in
     // the sequence attribute. It depends on complementMap attribute.
     unordered_map<char, int> count();
-
-    // Method returns reversed complementary sequence
     string get_rev_comp();
-
-    //Method returns the complementary sequence
     string get_complementary();
 };
 
